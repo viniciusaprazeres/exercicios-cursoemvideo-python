1041aef4ce300179cdf6dbcb76105029c572a9c7
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-08 18:35:33 -0300

    Dobro, Triplo, Raiz Quadrada

---
+++ scripts/ex006.py
@@ -0,0 +1,3 @@
+numero = int(input('Digite um número: '))
+
+print(f'O dobro de {numero} vale {numero * 2}. \nO triplo de {numero} é {numero * 3}. \nA raíz quadrada de {numero} é {pow(numero, (1/2)):.2f}')
