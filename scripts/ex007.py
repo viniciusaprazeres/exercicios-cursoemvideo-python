7dc4338ceec1132a730630b582f6ad8d89c78b35
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-08 18:36:55 -0300

    Média Aritmética

---
+++ scripts/ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+
+print(f'A média entre {nota1} e {nota2} é {(nota1 + nota2)/2:.1f}.')
