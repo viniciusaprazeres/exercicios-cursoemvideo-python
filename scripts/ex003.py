3520e58389e1f72a4224524a2c9e5f89163bae8a
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-08 08:41:36 -0300

    Somando dois números

---
+++ scripts/ex003.py
@@ -0,0 +1,6 @@
+valor1 = int(input('Digite um valor: '))  #Recebe um número.
+valor2 = int(input('Digite outro valor: '))  #Recee um segundo número.
+
+soma = valor1 + valor2  #Realiza a soma dos números recebidos.
+
+print(f'A soma entre {valor1} e {valor2} é {soma}!')  #Imprime na tela a soma entre os dois números.
