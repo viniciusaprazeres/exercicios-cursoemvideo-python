d5954345c26a7ee86eabdd33889a08474242aef0
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-09 16:53:54 -0300

    Conversor de Medidas

---
+++ scripts/ex008.py
@@ -0,0 +1,8 @@
+metros = float(input('Digite uma distância em metros: '))
+
+print(f'A distância em quilômetros é de {metros/1000}km.')
+print(f'A distância em hectômetros é de {metros/100}hm.')
+print(f'A distância em decâmetros é de {metros/10}dam.')
+print(f'A distância em decímetros é de {metros * 10:.0f}dm.')
+print(f'A distância em centímetros é de {metros * 100:.0f}cm.')
+print(f'A distância em milímetros é de {metros * 1000:.0f}mm.')
