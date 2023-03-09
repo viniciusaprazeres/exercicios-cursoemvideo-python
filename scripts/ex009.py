aafa8075433ae6902fa8693280e588bb49b2d447
viniciusaprazeres <viniciusaprazeres@gmail.com>
2023-03-09 16:54:27 -0300

    Tabuada

---
+++ scripts/ex009.py
@@ -0,0 +1,19 @@
+numero = int(input(f'Digite um número para saber a tabuada: '))
+
+print(15 * '-')
+print(f'{numero} x {1:2} = {numero * 1:2}')
+print(f'{numero} x {2:2} = {numero * 2:2}')
+print(f'{numero} x {3:2} = {numero * 3:2}')
+print(f'{numero} x {4:2} = {numero * 4:2}')
+print(f'{numero} x {5:2} = {numero * 5:2}')
+print(f'{numero} x {6:2} = {numero * 6:2}')
+print(f'{numero} x {7:2} = {numero * 7:2}')
+print(f'{numero} x {8:2} = {numero * 8:2}')
+print(f'{numero} x {9:2} = {numero * 9:2}')
+print(f'{numero} x {10:2} = {numero * 10:2}')
+print(15 * '-')
+
+
+
+data = True
+
