e94c45a97a1b06b095c7e54b7accdeffbeb07a9b
Vinicius Prazeres <viniciusaprazeres@gmail.com>
2023-09-21 12:35:31 -0300

    Classificando Atletas

---
+++ scripts/ex041.py
@@ -0,0 +1,19 @@
+ano_nascimento = int(input('Digite o ano de nascimento do atleta: '))
+ano_atual = 2017
+idade = ano_atual - ano_nascimento
+
+print(f'O atleta tem {idade} anos de idade.')
+
+
+if idade <= 9:
+    print('Classificação: MIRIM.')
+elif 9 < idade <= 14:
+    print('Classificação: INFANTIL.')
+elif 14 < idade <= 19:
+    print('Classificação: JUNIOR.')
+elif 19 < idade <= 25:
+    print('Classificação: SÊNIOR.')
+elif idade > 25:
+    print('Classificação: MASTER.')
+else:
+    print('Erro! Digite um ano válido.')
